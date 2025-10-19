1fed78c7b167e8ead920edfb66b1ad10d364b65d
PiotrKaszuba <piotrkaszuba1996@gmail.com>
2025-10-19 10:38:28 +0200

    ActionChat tracks selected unit uid so that replays can select the correct unit at the execution time - so that selected units dont have to be tracked nor consistent -> player/observer can select units for inspection

--- src/actions/action_chat.gd
+++ src/actions/action_chat.gd
@@ -1,10 +1,17 @@
 class_name ActionChat
 
 
-static func make(chat_message_arg: String) -> Action:
+static func make(chat_message_arg: String, selected_unit_uid: int) -> Action:
 	var action: Action = Action.new({
 		Action.Field.TYPE: Action.Type.CHAT,
 		Action.Field.CHAT_MESSAGE: chat_message_arg,
+		
+		# this isn't needed for immediate execution
+		# but is needed for replaying chat commands such as autooil
+		# without tracking selected unit
+		# so that player/observer can select units to inspect
+		# without interfering with replay
+		Action.Field.UID: selected_unit_uid
 		})
 
 	return action
